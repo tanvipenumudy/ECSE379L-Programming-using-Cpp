1c6811199d10ed557118f81fcfe3b770d6a23e67
Tanvi Penumudy <46785609+tanvipenumudy@users.noreply.github.com>
2021-03-02 22:15:32 +0530

    Added Lab-6

---
+++ Lab 6/E18CSE187_Lab6/E18CSE187_Lab6_Q3.cpp
@@ -0,0 +1,16 @@
+/*
+ * Q3.cpp
+ *
+ *  Created on: 28-Feb-2021
+ *      Author: Tanvi Penumudy (E18CSE187)
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<(n*(n+1))/2;
+    return 0;
+}
